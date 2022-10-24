ac6476c1204397c969f9fabb2e84e3a171f0e8e5
Nichita Trifan <nikiton254@gmail.com>
2022-10-24 10:05:03 -0500

    fixed statuc initialization

--- src/program1.cpp
+++ src/program1.cpp
@@ -6,5 +6,10 @@ using namespace std;
 int main(int ac, char **av)
 {
 	cout << testTable.ra1 << endl;
+
+	for (int i = 0; i < testTable.durationSize; i++) {
+		cout<< testTable.duration[i] << endl;
+	}
+
 	return 0;
 }
--- src/scripts/json_to_header.py
+++ src/scripts/json_to_header.py
@@ -10,34 +10,52 @@ with open(sys.argv[1], "r") as table_file:
 	table_dict : dict = json.load(table_file)
 
 with open(sys.argv[2], "w") as header_file:
-    header_file.writelines(preprocess_list)
-    #header_file.writelines(structure_def)
 
-    header_file.write("ValveTable testTable = {\n")
+    primitive_values = ""
+    array_values = ""
+    array_sizes = ""
+    reference_to_arr = ""
+
     for table_key in table_dict:
 
         if (table_key == "ra1" or
             table_key == "ra2" or
             table_key == "lobe_sep"):
-            header_file.write("\t" + str(table_dict[table_key]) + ",\n")
+            primitive_values += "\t" + str(table_dict[table_key]) + ",\n"
 
         elif (table_key == "duration" or
               table_key == "open" or
               table_key == "close" or
               table_key == "lobe_cl"):
-            #arr_len = len(table_dict[table_key])
+            array_sizes += "\t" + str(len(table_dict[table_key])) + ",\n"
+            reference_to_arr += "\t" + table_key + ",\n"
 
-            value_str = " { "
+            value_str = "static const int " + table_key + "[] = { "
             for value in table_dict[table_key]:
                 value_str += str(value) + ","
-            value_str += " },\n"
-            header_file.write("\tnew int[]" + value_str)
+            value_str = value_str[:-1]
+            value_str += " };\n"
+            array_values += value_str
 
         elif table_key == "lift":
-            value_str = " { "
+            array_sizes += "\t" + str(len(table_dict[table_key])) + ",\n"
+            reference_to_arr += "\t" + table_key + ",\n"
+
+            value_str = "static const double " + table_key + "[] = { "
             for value in table_dict[table_key]:
                 value_str += str(value) + ","
-            value_str += " },\n"
-            header_file.write("\tnew double[]" + value_str)
+            value_str = value_str[:-1]
+            value_str += " };\n"
+            array_values += value_str
 
+    initialization = "\nValveTable testTable = {\n"
+    array_sizes = array_sizes[:-2]
+    array_sizes += "\n"
+
+    header_file.writelines(preprocess_list)
+    header_file.write(array_values)
+    header_file.write(initialization)
+    header_file.write(primitive_values)
+    header_file.write(reference_to_arr)
+    header_file.write(array_sizes)
     header_file.write("};\n")
