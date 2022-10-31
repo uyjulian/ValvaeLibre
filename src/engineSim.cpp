364157d30fb7c565a52d816f25a7fe36535e2ac1
Jason Shick <viperb5@gmail.com>
2022-10-31 15:37:03 -0500

    Temp

--- src/engineSim.cpp
+++ src/engineSim.cpp
@@ -10,6 +10,8 @@ int RPM = 60;
 using namespace std;
 
 int waitTime(int);
+void CPS(toneWheel, int);
+void signalOut(int, bool);
 
 struct toneWheel
 {
@@ -29,4 +31,18 @@ int waitTime(int tableEntries) {
 	double RPS = RPM / 60;
 	int nanoSleep = round((RPS / tableEntries) * 1000000000);
 	return nanoSleep;
+}
+
+void CPS(toneWheel tw, int sleepTime) {
+	while (true) {
+		int tooth = 1;
+		for (int i = 0; i < 720; i++) {
+			if (i % tw.degOfSeperation < (tw.degOfSeperation / 2)) signalOut(55000, true);
+			else signalOut(55000, false);
+		}
+	}
+}
+
+void signlOut(int port,bool on) {
+	
 }
