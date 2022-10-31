04192361f336f162a462c5042e7c4c722124728e
Nichita Trifan <nikiton254@gmail.com>
2022-10-31 10:45:05 -0500

    defined bit masks for 4 cylinder engine

--- src/program1.cpp
+++ src/program1.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int main(int ac, char **av)
 {
-	for (int i = 0; i < testTable.table_size; i++) {
-		cout<< testTable.table[i] << endl;
-	}
+	cout << masks::BIT_32_MASK << endl;
+	cout << (masks::BIT_32_MASK & test_table.table[0]) << endl;
+	cout << !(masks::BIT_32_MASK & test_table.table[0]) << endl;
+
+	//for (int i = 0; i < test_table.table_size; i++) {
+	//	cout<< test_table.table[i] << endl;
+	//}
 
 	return 0;
 }
