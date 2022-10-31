1e5def899145b28537acbf191378e00fbe734f24
Jason Shick <jlshick1@buffs.wtamu.edu>
2022-10-31 00:20:48 -0500

    Insert toneWheel construct and waitTime()

--- src/engineSim.cpp
+++ src/engineSim.cpp
@@ -1,11 +1,32 @@
 
 #include <stdio.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "table.h"
+
+int RPM = 60;
+
+using namespace std;
+
+int waitTime(int);
+
+struct toneWheel
+{
+	int numOfTeeth = 35;
+	int degOfSeperation = 10;
+	int missingTooth = 36;
+
+};
 
 int main(int ac, char** av)
 {
-	(void)ac;
-	(void)av;
+	int sleepTime = waitTime(720);
+	//cout << sleepTime;
+}
 
-	fprintf(stdout, "%s\n", "Hello World from Amazing program 3!");
-	return 0;
+int waitTime(int tableEntries) {
+	double RPS = RPM / 60;
+	int nanoSleep = round((RPS / tableEntries) * 1000000000);
+	return nanoSleep;
 }
