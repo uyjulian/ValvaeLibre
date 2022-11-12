d67609af60a2c84dea6018e1d81eaf3489ce82e3
Nichita Trifan <nikiton254@gmail.com>
2022-11-11 21:27:18 -0600

    changed error handling

--- src/table.h
+++ src/table.h
@@ -1,6 +1,12 @@
 #ifndef __TABLE_H__ 
 #define __TABLE_H__
 
+#include <stdio.h>
+#define __STDIO_H__
+
+#include <cstdlib>
+#define  __CSTDLIB__
+
 struct ValveTable {
 	const unsigned int* table;
 	int table_size;
@@ -10,10 +16,14 @@ struct ValveTable {
 extern ValveTable test_table;
 
 static inline bool check_bit(unsigned char bit, unsigned short angle) {
-	if (angle > 719)
-		throw "\"angle\" must be positive and less than 720!";
-	if (bit > 31)
-		throw "\"bit\" must be positive less than 32!";
+	if (angle > 719) {
+		printf("\"angle\" value must be less than 720!");
+		exit(EXIT_FAILURE);
+	}
+	if (bit > 31) {
+		printf("\"bit\" value must be less than 32!");
+		exit(EXIT_FAILURE);
+	}
 	return test_table.table[angle] & (1 << bit);
 };
 
