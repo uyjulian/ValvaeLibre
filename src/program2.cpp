1c2b58b0e3feb0d6d9c1440d598aa821152b66ab
Jason Shick <jlshick1@buffs.wtamu.edu>
2022-11-29 09:58:12 -0600

    Save

--- src/program2.cpp
+++ src/program2.cpp
@@ -1,11 +1,22 @@
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#ifdef COMPILING_FOR_RASPI_PICO
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/multicore.h"
+#else
+//#include <unistd.h>
+#endif
+
 
 int main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
+	
 
-	fprintf(stdout, "%s\n", "Hello World from Program 2!");
 	return 0;
 }
