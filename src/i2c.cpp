af30fbee39e6d9d1b0f4932860b8d423195d037b
nichitatrifan <nichita.trifanmd@gmail.com>
2023-02-10 17:27:59 -0600

    added pico guards

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <string>
+
+#ifdef COMPILING_FOR_RASPI_PICO
+
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
+
+#endif
 
 #define SDA_PIN 0
 #define SCL_PIN 1
@@ -55,12 +60,15 @@ const static int LCD_ADDR = 0x27;
 /*---------------------------------------------------------------------*/
 
 // Single byte transfer function
+#ifdef COMPILING_FOR_RASPI_PICO
 void i2c_write_byte(uint8_t val) {
 #ifdef i2c_default
     i2c_write_blocking(i2c_default, LCD_ADDR, &val, 1, false);
 #endif
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 void lcd_toggle_enable(uint8_t val) {
     // Toggle enable pin on LCD display
     // We cannot do this too quickly or things don't work
@@ -70,7 +78,9 @@ void lcd_toggle_enable(uint8_t val) {
     i2c_write_byte(val & ~LCD_ENABLE_BIT);
     sleep_us(DELAY_US);
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 // The display is sent a byte as two separate nibble transfers
 void lcd_send_byte(uint8_t val, int mode) {
     // mode: 1 or 0 (sending char or a command)
@@ -82,11 +92,15 @@ void lcd_send_byte(uint8_t val, int mode) {
     i2c_write_byte(low);
     lcd_toggle_enable(low);
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 void lcd_clear(void) {
     lcd_send_byte(LCD_CLEARDISPLAY, LCD_COMMAND);
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 void lcd_init() {
     lcd_send_byte(0x03, LCD_COMMAND);
     lcd_send_byte(0x03, LCD_COMMAND);
@@ -98,23 +112,31 @@ void lcd_init() {
     lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
     lcd_clear();
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 static void inline lcd_send_char(char val) {
     lcd_send_byte(val, LCD_CHARACTER);
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 void lcd_send_string(const std::string str, size_t str_len) {
     for (size_t i = 0; i < str_len; i++){
 		lcd_send_byte(str.at(i), LCD_CHARACTER);
 	}
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 // go to location on LCD
 void lcd_set_cursor(int line, int position) {
     int val = (line == 0) ? 0x80 + position : 0xC0 + position;
     lcd_send_byte(val, LCD_COMMAND);
 }
+#endif
 
+#ifdef COMPILING_FOR_RASPI_PICO
 int main() {
     stdio_init_all();
     i2c_init(i2c_default, 100 * 1000);
@@ -146,3 +168,8 @@ int main() {
     }
     return 0;
 }
+#else
+int main() {
+	return 0;
+}
+#endif
