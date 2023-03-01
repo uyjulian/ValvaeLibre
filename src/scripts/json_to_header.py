8cb32eb1c0255733d99635e3594f7ef73dfa2ea6
nichitatrifan <nichita.trifanmd@gmail.com>
2023-02-28 20:21:31 -0600

    added 1 cylinder and 2 cylinder

--- src/scripts/json_to_header.py
+++ src/scripts/json_to_header.py
@@ -1,11 +1,13 @@
 import json
 import sys
-# -------------- BITS FOR VALVES ---------------------------
+# -------------- BITS FOR VALVES -------------------------------
 INTAKE_BITS = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 EXHAUST_BITS = (16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)
 
 # -------------- VALVES OFFSETS FOR DIFFERENT ENGINES ----------
 offsets = {
+    1: 720,
+    2: 360,
     4: 180,
     6: 120,
     8: 90
@@ -15,11 +17,11 @@ offsets = {
 def set_bit(number : int, bit : int) -> int:
     return number | (1<<bit)
 
-# -------------- TABLE -----------------------------------
+# -------------- TABLE ---------------------------------------
 TABLE_SIZE = 720
 my_table = [0b00000000000000000000000000000000] * TABLE_SIZE
 
-# -------------- READING JSON VALUES---------------------
+# -------------- READING JSON VALUES--------------------------
 def read_json_keys(extract_keys: dict):
     with open(sys.argv[1], "r") as table_file:
         table_dict : dict = json.load(table_file)
@@ -28,7 +30,7 @@ def read_json_keys(extract_keys: dict):
         if table_key in extract_keys:
             extract_keys[table_key] = table_dict[table_key]
 
-# -------------- COMPUTING THE VALUES --------------------
+# -------------- COMPUTING THE VALUES -----------------------
 def computing_values(my_table: list, extract_keys: dict):
     current_offset = 0
     for cylinder in extract_keys["firing_order"]:
@@ -46,7 +48,7 @@ def computing_values(my_table: list, extract_keys: dict):
             my_table[angle_index] = set_bit(my_table[angle_index], bit)
         current_offset += offsets[extract_keys["number_of_cylinders"]]
 
-# -------------- CREATING STRINGS FOR TABLE -----------
+# -------------- CREATING STRINGS FOR TABLE ------------------
 def create_table(my_table:list) -> str:
     array_init = "static const unsigned int table[] = {\n"
     for i, val in enumerate(my_table):
@@ -60,14 +62,14 @@ def create_table(my_table:list) -> str:
     array_init += "\n};\n"
     return array_init
 
-# -------------- WRITING THE table.cpp FILE --------------
+# -------------- WRITING THE table.cpp FILE -------------------
 def writting_strings(preprocess_list: list, array_init: str, table_init: str):
     with open(sys.argv[2], "w") as header_file:
         header_file.writelines(preprocess_list)
         header_file.write(array_init)
         header_file.writelines(table_init)
 
-# -------------- FUNCTION FOR PRINTING TABLES -----------
+# -------------- FUNCTION FOR PRINTING TABLES ----------------
 def print_table(my_table: list):
     bin_table = list(map(lambda val: bin(val), my_table))
     print(bin_table)
